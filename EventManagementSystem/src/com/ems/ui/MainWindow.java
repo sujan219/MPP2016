d3634a7d3c5491db6ca7c681b5ece1aa35494f9b
Sujan Shrestha <sujan.shrestha219@gmail.com>
2016-05-31 11:26:50 -0500

    new changes

--- EventManagementSystem/src/com/ems/ui/MainWindow.java
+++ EventManagementSystem/src/com/ems/ui/MainWindow.java
@@ -1,13 +1,25 @@
 package com.ems.ui;
 
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainWindow extends Stage{
 	private Stage primaryStage;
 	public MainWindow(Stage aPrimaryStage){
 		primaryStage = aPrimaryStage;
-		
-		setTitle("Event management system");
-		
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+			setTitle("Event management system");
+			
+			setScene(new Scene(root));
+			show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
