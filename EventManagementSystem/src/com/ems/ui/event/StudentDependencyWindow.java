0d8d770199267c2b8a81f8496a462d6f6c3eb82d
nishantbalami <nishantbalami7@gmail.com>
2016-06-01 16:20:46 -0500

    Commit!

--- EventManagementSystem/src/com/ems/ui/event/StudentDependencyWindow.java
+++ EventManagementSystem/src/com/ems/ui/event/StudentDependencyWindow.java
@@ -1,10 +1,14 @@
 package com.ems.ui.event;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import com.ems.baseclasses.DataObject;
 
-import javafx.scene.control.Label;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 
@@ -18,13 +22,26 @@ public class StudentDependencyWindow extends AbstractEventDependenciesWindow{
 	protected Pane getEachPane(int index) {
 		Pane pane = new Pane();
 		HBox hbox = new HBox();
-		hbox.getChildren().addAll(new Label("test1"), new Label("test2"));
+		CheckBox chk = new CheckBox();
+		chk.setId("checkBox_" + index);
+		hbox.getChildren().addAll(new TextField(), chk);
 		pane.getChildren().add(hbox);
 		return pane;
 	}
 
 	@Override
 	protected List<DataObject> getSelectedObjects() {
-		return null;
+		ObservableList<Node> paneList = getPaneList();
+		List<DataObject> listObject = new ArrayList<DataObject>();
+		for(int i = 0; i < paneList.size(); i++)
+		{
+			Node node = paneList.get(i);
+			CheckBox chk = (CheckBox)node.lookup("#checkBox_" + i);
+			if(chk.isSelected())
+			{
+				listObject.add(getItem(i));
+			}
+		}
+		return listObject;
 	}
 }
